84bb234dcc95466482f4cb69dfed609d6173f144
KalKid458 <147622281+KalKid458@users.noreply.github.com>
2023-10-11 15:07:49 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+document.getElementById("display1").onclick = function(){
+    count=String
+    count=986083911;
+    document.getElementById("phone").innerHTML =count;
+}
+
+document.getElementById("display2").onclick = function(){
+    count=String
+    count=986083911;
+    document.getElementById("WebDEV").innerHTML =count;
+}
+
+document.getElementById("display3").onclick = function(){
+    count=String
+    count=986083911;
+    document.getElementById("SQL1").innerHTML =count;
+}
